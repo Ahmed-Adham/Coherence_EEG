0f07d3383b5290e965fb44d9da0e361d231eb641
Ahmed Adham <98322189+Ahmed-Adham@users.noreply.github.com>
2022-07-24 18:07:21 +0200

    Update Coherence_FFT.m

--- Coherence_FFT.m
+++ Coherence_FFT.m
@@ -1,32 +1,34 @@
-%% Fonction pour le Calcul de la cohérence Cortico-musculaire par FFT
+%% Fonction pour le Calcul de la cohÃ©rence Cortico-musculaire par FFT
 %% Ahmed ADHAM - 2022
 
 %%
-% cette fonction va chercher les fichiers avec "previb" ou "postvib" dans leur nom et calculer la cohérence
-% Les électrodes d'intérêt pour l'EEG sont les électrodes entre 7 et 70 dans le fichier
-% 
+% Cette fonction va chercher les dossiers les fichiers contenant "data" dans leur nom calculer la cohÃ©rence.
+% Il faut spÃ©cifier le chemin des fichier prÃ©-vibration et des fichiers post-vibration ! 
+% Les Ã©lectrodes d'intÃ©rÃªt pour l'EEG sont les Ã©lectrodes entre 7 et 70 dans le fichier
+% Une fois la cohÃ©rence calculÃ©e elle est enregistrÃ©e dans la variable COH_save 
+% que tu peux sauvegarder / exporter pour des stats par la suite 
 %%
 
 clear 
 
-load('EEG_Struc_64.mat') % charger noms électrodes + positions
+load('EEG_Struc_64.mat') % charger noms Ã©lectrodes + positions
 %
-%% définition des paramètres
+%% dÃ©finition des paramÃ¨tres
 
 
-srate = 1024 %Hz - Fréquence d'échantillonage en Hertz 
-Interval_1 = [4000:8000] % Interval de temps où calculer la cohérence dans le signal
-repertoire_1 = '/Users/Ahmed/Downloads/wetransfer_s7j1contract2b_interpbad_notch_band_2022-07-21_1633/pre/'; % Répertoire où se trouvent les .mat Pré-vibration
-repertoire_2 = '/Users/Ahmed/Downloads/wetransfer_s7j1contract2b_interpbad_notch_band_2022-07-21_1633/post/'; % Répertoire où se trouvent les .mat Post-vibration
+srate = 1024 %Hz - FrÃ©quence d'Ã©chantillonage en Hertz 
+Interval_1 = [4000:8000] % Interval de temps oÃ¹ calculer la cohÃ©rence dans le signal
+repertoire_1 = '/Users/Ahmed/Downloads/wetransfer_s7j1contract2b_interpbad_notch_band_2022-07-21_1633/pre/'; % RÃ©pertoire oÃ¹ se trouvent les .mat PrÃ©-vibration
+repertoire_2 = '/Users/Ahmed/Downloads/wetransfer_s7j1contract2b_interpbad_notch_band_2022-07-21_1633/post/'; % RÃ©pertoire oÃ¹ se trouvent les .mat Post-vibration
 EMG_chanel = 6; % Quel canal EMG utiliser : mettre 6 pour FCR, 5 pour ECR 
-Freq_space = [1:0.5:50]; % Espace de fréquences à étudier (1 à 50Hz tous les 0.5Hz). Attention au temps de calcul ! 
+Freq_space = [1:0.5:50]; % Espace de frÃ©quences Ã  Ã©tudier (1 Ã  50Hz tous les 0.5Hz). Attention au temps de calcul ! 
 Utiliser_laplacien = 1; %0 = pas de laplacien ; 1 = laplacien
 condition{1} = 'pre-vibration' % nom condition_1
 condition{2} = 'post-vibration'% nom condition_2
-Hanning_window = 1000; % La cohérence par FFT demande une fenêtre d'ordre du filtre : 
-% c'est un paramètre un peu arbitraire qui détermine la résultion de
-% fréquence ... (trop (ex 2000) sera illisible, pas assez (ex 100) sera tout
-% mélangé; du coup j'ai mit 1000 mais libre à chacun de changer
+Hanning_window = 1000; % La cohÃ©rence par FFT demande une fenÃªtre d'ordre du filtre : 
+% c'est un paramÃ¨tre un peu arbitraire qui dÃ©termine la rÃ©sultion de
+% frÃ©quence ... (trop (ex 2000) sera illisible, pas assez (ex 100) sera tout
+% mÃ©langÃ©; du coup j'ai mit 1000 mais libre Ã  chacun de changer
 
 
 %% c'est parti :) ! 
@@ -50,7 +52,7 @@ file_to_load = strcat(repertoire_2,Name_file{pre_post}(file).name);
 load(file_to_load);
 end
 
-EEG(:,:) = F(7:70,:); %% Si ca bug ici c'est que Brainstorm a changé le nom d'enregistement des données EEG, préviens moi ! 
+EEG(:,:) = F(7:70,:); %% Si ca bug ici c'est que Brainstorm a changÃ© le nom d'enregistement des donnÃ©es EEG, prÃ©viens moi ! 
 EMG = F(EMG_chanel,:);
 
 if Utiliser_laplacien == 0 
@@ -62,7 +64,7 @@ end
 
 times = 0:1/1000:size(EEG_Ana,2)/1000; % temps
 
-%% Calcul de la cohérence
+%% Calcul de la cohÃ©rence
 
 for chan = 1:64
 [coh(file,chan,:),frex] = mscohere(EEG_Ana(chan,Interval_1),EMG(Interval_1),hann(Hanning_window),[],Freq_space,srate);
@@ -73,45 +75,45 @@ end
 COH_save{pre_post}(:,:,:) = coh;
 
 end
-% calculer la cohérence à partir de la transformée de Fourrier.
-% J'ai fait une fonction qui la calcule à partir des ondelettes mais je ne
-% suis pas sur du résultat et c'est beaaaucuoup plus long ... on pourra
-% essayer mais à voir déjà si ça marche (la cohérence par ondelette n'est
+% calculer la cohÃ©rence Ã  partir de la transformÃ©e de Fourrier.
+% J'ai fait une fonction qui la calcule Ã  partir des ondelettes mais je ne
+% suis pas sur du rÃ©sultat et c'est beaaaucuoup plus long ... on pourra
+% essayer mais Ã  voir dÃ©jÃ  si Ã§a marche (la cohÃ©rence par ondelette n'est
 % pas de base dans MatLab) 
 
-%% Paramètres d'affichage des résultats 
+%% ParamÃ¨tres d'affichage des rÃ©sultats 
 
 
-ylim_d = [-0.2 0.2]; % échelle cohérence figure de différence (figure 100)
-ylim_c = [0 0.3]; % échelle cohérence figure individuelle (figure 1/2)
-c_lim = [0 0.3]; % échelle des couleurs entre 0 et 1 pour les cartes globales (figure 1/2)
+ylim_d = [-0.2 0.2]; % Ã©chelle cohÃ©rence figure de diffÃ©rence (figure 100)
+ylim_c = [0 0.3]; % Ã©chelle cohÃ©rence figure individuelle (figure 1/2)
+c_lim = [0 0.3]; % Ã©chelle des couleurs entre 0 et 1 pour les cartes globales (figure 1/2)
 
-f1 = 8;  % Fréquence affichée pour la figure 1
-f2 = 12; % Fréquence affichée pour la figure 2
-f3 = 20; % Fréquence affichée pour la figure 3
-f4 = 30; % Fréquence affichée pour la figure 4
+f1 = 8;  % FrÃ©quence affichÃ©e pour la figure 1
+f2 = 12; % FrÃ©quence affichÃ©e pour la figure 2
+f3 = 20; % FrÃ©quence affichÃ©e pour la figure 3
+f4 = 30; % FrÃ©quence affichÃ©e pour la figure 4
 
-%% Affichage Différence
+%% Affichage DiffÃ©rence
 
 
-%électrode sur laquelle calculer la différence de cohérence (32 = C3, cf tableau en bas) 
+%Ã©lectrode sur laquelle calculer la diffÃ©rence de cohÃ©rence (32 = C3, cf tableau en bas) 
 Electrode_diff = 32; 
 
-% Différence de Cohérence sur Electrode_diff entre post-pré
+% DiffÃ©rence de CohÃ©rence sur Electrode_diff entre post-prÃ©
 figure(100)
 plot(frex,squeeze(mean(COH_save{2}(:,Electrode_diff,:),1))-squeeze(mean(COH_save{1}(:,Electrode_diff,:),1)))
 ylim(ylim_d)
-title([ 'Différence de cohérence : Post-Pré , ' num2str(EEG_64.chanlocs(Electrode_diff).labels)]) % dans l'exemple entre 1 et 4 secondes
+title([ 'DiffÃ©rence de cohÃ©rence : Post-PrÃ© , ' num2str(EEG_64.chanlocs(Electrode_diff).labels)]) % dans l'exemple entre 1 et 4 secondes
 grid on
 h_a = figure(100);
 name_a = ['difference electrode' num2str(EEG_64.chanlocs(Electrode_diff).labels) ];
 saveas(h_a,name_a,'jpg')
 
 
-%% Affichage cohérence pour chaque groupe
-% affichage de la cohérence 
-% en haut : cohérence sur Electrode_diff
-% en dessous = carte corticale pour chaque groupe aux fréquences spécifiées
+%% Affichage cohÃ©rence pour chaque groupe
+% affichage de la cohÃ©rence 
+% en haut : cohÃ©rence sur Electrode_diff
+% en dessous = carte corticale pour chaque groupe aux frÃ©quences spÃ©cifiÃ©es
 
 
 for pre_post = 1:2
@@ -119,7 +121,7 @@ for pre_post = 1:2
 figure(pre_post)
 subplot(3,2, [1 2])
 plot(frex,squeeze(mean(COH_save{pre_post}(:,Electrode_diff,:),1)))
-title([ 'Cohérence ' condition{pre_post} ' ' num2str(EEG_64.chanlocs(Electrode_diff).labels)]) % dans l'exemple entre 1 et 4 secondes
+title([ 'CohÃ©rence ' condition{pre_post} ' ' num2str(EEG_64.chanlocs(Electrode_diff).labels)]) % dans l'exemple entre 1 et 4 secondes
 ylim(ylim_c)
 
 
