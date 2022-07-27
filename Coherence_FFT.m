cba858b8604d3a577b9d6a99e2166b870cae5185
Ahmed Adham <98322189+Ahmed-Adham@users.noreply.github.com>
2022-07-27 17:49:21 +0200

    Update Coherence_FFT.m

--- Coherence_FFT.m
+++ Coherence_FFT.m
@@ -6,7 +6,7 @@
 % Les électrodes d'intérêt pour l'EEG sont les électrodes entre 7 et 70 dans le fichier
 % 
 %%
-clear 
+clear
 
 load('EEG_Struc_64.mat') % charger noms électrodes + positions
 %
@@ -18,22 +18,23 @@ repertoire_1 = '/Users/Ahmed/Downloads/wetransfer_s7j1contract2b_interpbad_notch
 repertoire_2 = '/Users/Ahmed/Downloads/wetransfer_s7j1contract2b_interpbad_notch_band_2022-07-21_1633/post/'; % Répertoire où se trouvent les .mat Post-vibration
 EMG_chanel = 6; % Quel canal EMG utiliser : mettre 6 pour FCR, 5 pour ECR 
 Freq_space = [1:0.5:50]; % Espace de fréquences à étudier (1 à 50Hz tous les 0.5Hz). Attention au temps de calcul ! 
-Utiliser_laplacien = 1; %0 = pas de laplacien ; 1 = laplacien // je conseille de ne pas le mettre mais à toi de voire
+Utiliser_laplacien = 0; %0 = pas de laplacien ; 1 = laplacien // je conseille de ne pas le mettre mais à toi de voire
 condition{1} = 'pre-vibration' % nom condition_1
 condition{2} = 'post-vibration'% nom condition_2
-n_sujet = 2; % nombre de sujets dans le protocole
+n_sujet = 1; % nombre de sujets dans le protocole
+n_analyse = 10; % nombre d'analyses
 Hanning_window = 1000; % La cohérence par FFT demande une fenêtre d'ordre du filtre : 
 % c'est un paramètre un peu arbitraire qui détermine la résultion de
 % fréquence ... (trop (ex 2000) sera illisible, pas assez (ex 100) sera tout
 % mélangé; du coup j'ai mit 1000 mais libre à chacun de changer
 % j'ai mit un petit truc en plus pour simuler une rythme à 10Hz sur C3 et sur le canal EMG ci dessous
 % : 
-% par défaut laisse sig à 0, sauf si tu veux simuler; 
+% par défaut laisse sim à 0, sauf si tu veux simuler; 
 % Tu peux changer la puissance du signal simulé en jouant sur le 0.000001
 % essaie egalement de voire ce que ca donne avec et sans filtre laplacien
 % et essaie ensuite de changer le hanning_window ;) 
 
-sim = 1;
+sim = 0;
 power_sim = 0.0001;
 
 %% Paramètres d'affichage des résultats 
@@ -51,35 +52,32 @@ f3 = 20; % Fréquence affichée pour la figure 3
 f4 = 30; % Fréquence affichée pour la figure 4
 
 
+
+
 %% c'est parti :) ! 
 
 for sujet = 1:n_sujet
 
 for pre_post = 1:2 
         
-    
 repertoire_1s = [repertoire_1 'sujet_' num2str(sujet) '/'] 
 repertoire_2s = [repertoire_2 'sujet_' num2str(sujet) '/'] 
 
-Name_file{n_sujet,1} = dir(fullfile(repertoire_1s, 'data*'))
-Name_file{n_sujet,2} = dir(fullfile(repertoire_2s, 'data*'))
+Name_file{sujet,1} = dir(fullfile(repertoire_1s, 'data*'))
+Name_file{sujet,2} = dir(fullfile(repertoire_2s, 'data*'))
 
 
-% for file = 1:2 % met ça quand tu es sur de la simulation si tu ne veux pas qu'il calcule pour les 30 sujets) :) 
-% à la place de : 
-% for file = 1:length(Name_file{n_sujet,pre_post})   (ligne 73)
+for file = 1:n_analyse 
 
-for file = 1:length(Name_file{n_sujet,pre_post}) 
-
-disp(strcat({'Il reste : ' num2str(file),' fichier / ', num2str(length(Name_file{n_sujet,pre_post})) 'sujet'  num2str(pre_post)}))
+disp(strcat({'Il reste : ' num2str(file),' fichier / ', num2str(n_analyse) 'sujet'  num2str(pre_post)}))
 
 if pre_post == 1
-file_to_load = strcat(repertoire_1s,Name_file{n_sujet,1}(file).name);
+file_to_load = strcat(repertoire_1s,Name_file{sujet,1}(file).name);
 load(file_to_load);
 end
 
 if pre_post == 2
-file_to_load = strcat(repertoire_2s,Name_file{n_sujet,2}(file).name);
+file_to_load = strcat(repertoire_2s,Name_file{sujet,2}(file).name);
 load(file_to_load);
 end
 
@@ -115,10 +113,11 @@ end
 
 end
 
-COH_save{pre_post}(:,:,:,n_sujet) = coh;
+COH_save{pre_post}(:,:,:,sujet) = coh; % epoch % electrode % frex % subject
 
 end 
 end
+
 % calculer la cohérence à partir de la transformée de Fourrier.
 % J'ai fait une fonction qui la calcule à partir des ondelettes mais je ne
 % suis pas sur du résultat et c'est beaaaucuoup plus long ... on pourra
@@ -185,4 +184,3 @@ saveas(h_b,name_b,'jpg')
 h_c = figure(2);
 name_c = ['Coherence Post'];
 saveas(h_c,name_c,'jpg')
-
